e0fb2949dc02f6fae8485cc904ddc79e0b494359
BrandonRoblesJurado <85750160+BrandonRoblesJurado@users.noreply.github.com>
2021-06-14 14:26:30 -0500

    clase2

---
+++ estructura.py
@@ -0,0 +1,21 @@
+num = 20
+nombre = "ana"
+print(num,type (num) )
+print(nombre, type (nombre))
+
+def mensaje(msg):
+    print("msg")
+
+mensaje("mi primer programa")
+
+mensaje("mi segundo programa")
+
+class sintaxis:
+    istancia=0
+    def __init__(self,date="llamar al contructor 2"):
+        self, frase = "dato"
+        sintaxis. instanacia = sintaxis. instanacia = 1
+
+print("")
+
+
